90912b6ac148175034cc69149eb9ab577e36db76
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-07-11 15:58:05 -0700

    adjusted data types and interfaces correctly

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,6 +6,16 @@
  * TODO: fix data types.
  */
 
+import {
+  CompanyI,
+  JobI,
+  endpointT,
+  methodT,
+  handleT,
+  searchTermT,
+  jobIdT,
+} from "./definitions";
+
 const BASE_URL = "http://localhost:3001";
 // "https://backend-jobly.onrender.com" ||
 
@@ -15,7 +25,11 @@ class JoblyApi {
   // DON'T MODIFY THIS TOKEN
   static token = localStorage.getItem("token");
 
-  static async request(endpoint, data = {}, method = "GET") {
+  static async request(
+    endpoint: endpointT,
+    data = {},
+    method: methodT = "GET"
+  ) {
     const url = new URL(`${BASE_URL}/${endpoint}`);
     const headers = {
       authorization: `Bearer ${JoblyApi.token}`,
@@ -43,7 +57,7 @@ class JoblyApi {
 
   /** Get details on a company by handle. */
 
-  static async getCompany(handle) {
+  static async getCompany(handle: handleT): Promise<CompanyI> {
     let res = await this.request(`companies/${handle}`);
 
     return res.company;
@@ -57,7 +71,7 @@ class JoblyApi {
    */
 
   // static async getCompanies(searchTerm) {
-  static async getCompanies(searchTerm) {
+  static async getCompanies(searchTerm?: searchTermT) {
     let data = searchTerm ? { nameLike: searchTerm } : {};
     let res = await this.request("companies", data);
 
@@ -69,7 +83,7 @@ class JoblyApi {
    * Send query string if filtering term is passed in.
    */
 
-  static async getJobs(searchTerm) {
+  static async getJobs(searchTerm: searchTermT) {
     let data = searchTerm ? { title: searchTerm } : {};
     let res = await this.request("jobs", data);
 
@@ -81,59 +95,65 @@ class JoblyApi {
    * Returns { id, title, salary, equity, company }
    * where company is { handle, name, description, numEmployees, logoUrl }
    */
-  static async getJob(id) {
+  static async getJob(id: jobIdT): Promise<JobI> {
     let res = await this.request(`jobs/${id}`);
 
     return res.job;
   }
 
   /** login: takes in a username and password and returns a JWT */
-  static async login(userCreds) {
-    let res = await this.request("auth/token", userCreds, "POST");
-    this.token = res.token;
+  //TODO: FINISH WITH AUTHENTICATION.
+  // static async login(userCreds) {
+  //   let res = await this.request("auth/token", userCreds, "POST");
+  //   this.token = res.token;
 
-    return this.token;
-  }
+  //   return this.token;
+  // }
 
   /** register: takes in a user object and returns a JWT */
-  static async register(user) {
-    let res = await this.request("auth/register", user, "POST");
-    this.token = res.token;
+  //TODO: FINISH WITH AUTHENTICATION
+  // static async register(user) {
+  //   let res = await this.request("auth/register", user, "POST");
+  //   this.token = res.token;
 
-    return this.token;
-  }
+  //   return this.token;
+  // }
 
   /** Get info on a user by username */
-  static async getUser(username) {
-    let res = await this.request(`users/${username}`);
+  //TODO: FINISH WITH AUTHENTICATION
+  // static async getUser(username) {
+  //   let res = await this.request(`users/${username}`);
 
-    return res.user;
-  }
+  //   return res.user;
+  // }
 
   /** Patch a user's info, return the new user info. */
-  static async editUser(username, editFormData) {
-    let res = await this.request(`users/${username}`, editFormData, "PATCH");
+  //TODO: FINISH WITH AUTHENTICATION
+  // static async editUser(username, editFormData) {
+  //   let res = await this.request(`users/${username}`, editFormData, "PATCH");
 
-    return res.user;
-  }
+  //   return res.user;
+  // }
 
   /** Applies to a job given a username/job ID. Returns the Job ID */
-  static async applyToJob(username, jobId) {
-    let res = await this.request(`users/${username}/jobs/${jobId}`, {}, "POST");
+  //TODO: FINISH AFTER FEATURE IS IMPLEMENTED.
+  // static async applyToJob(username, jobId) {
+  //   let res = await this.request(`users/${username}/jobs/${jobId}`, {}, "POST");
 
-    return res.applied;
-  }
+  //   return res.applied;
+  // }
 
   /** Unapplies to a job given a username/job ID. Returns the Job ID that was unapplied. */
-  static async unapplyToJob(username, jobId) {
-    let res = await this.request(
-      `users/${username}/jobs/${jobId}`,
-      {},
-      "DELETE"
-    );
+  //TODO: FINISH AFTER FEATURE IS IMPLEMENTED.
+  // static async unapplyToJob(username, jobId) {
+  //   let res = await this.request(
+  //     `users/${username}/jobs/${jobId}`,
+  //     {},
+  //     "DELETE"
+  //   );
 
-    return res.unapplied;
-  }
+  //   return res.unapplied;
+  // }
 }
 
 export default JoblyApi;
--- src/utils/definitions.ts
+++ src/utils/definitions.ts
@@ -1,22 +1,44 @@
 /** Definitions and data types used throughout application. */
 
+/** Company and Job types/interfaces. */
+
 export interface CompanyI {
-  company: {
-    handle: string;
-    name: string;
-    description: string;
-    numEmployees: string;
-    logoUrl: string;
-  };
+  handle: string;
+  name: string;
+  description: string;
+  numEmployees: string;
+  logoUrl?: string;
+  jobs?: Array<JobI>;
 }
 
 export interface JobI {
-  job: {
-    id: number;
-    title: string;
-    salary?: string;
-    equity?: string;
-    companyHandle: string;
-    companyName: string;
-  };
+  id: number;
+  title: string;
+  salary?: string;
+  equity?: string;
+  companyHandle: string;
+  companyName: string;
+}
+
+/** Component types/interfaces. */
+
+export interface JobDetailsI {
+  job: JobI;
+}
+
+export interface CompanyCardI {
+  company: CompanyI;
+}
+
+/** API types/interfaces. */
+
+export type endpointT = string;
+export type dataT = {}; //TODO: FINISH WHEN WE FIND OUT WHAT IS INSIDE DATA.
+export type methodT = "GET" | "POST" | "DELETE";
+export type handleT = string | undefined;
+export type searchTermT = string;
+export type jobIdT = number;
+
+export interface getCompanyI {
+  res: string;
 }
