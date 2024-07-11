0cea8f8eca97d4db1b5c2e335a0532bfe671b7b0
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-07-11 15:31:10 -0700

    fixed data type issues

---
+++ src/containers/Job/JobDetail/index.tsx
@@ -0,0 +1,77 @@
+/** JobDetail.
+ *
+ * @description
+ * Card component that holds all job details.
+ *
+ * Props:
+ * - job: { id, title, salary, equity, companyHandle, companyName }
+ */
+
+// import { useState, useEffect } from "react";
+
+import { JobI } from "../../../utils/definitions";
+
+export default function JobDetail({ job }: JobI) {
+  // const [hasApplied, setHasApplied] = useState(false);
+
+  // useEffect(() => {
+  //   if (hasAppliedToJob(job.id)) {
+  //     setHasApplied(true);
+  //   }
+  // }, [job.id, hasAppliedToJob]);
+
+  // /** handles onClick for liking jobs. */
+  // async function handleApply(evt) {
+  //   const jobId = +evt.target.value;
+
+  //   try {
+  //     await apply(currentUser.data.username, jobId);
+  //     setHasApplied(true);
+  //   } catch (error) {
+  //     console.error("Error applying to job", error);
+  //   }
+  // }
+
+  // /** handles onClick for unliking jobs. */
+  // async function handleUnapply(evt) {
+  //   const jobId = +evt.target.value;
+
+  //   try {
+  //     await unapply(currentUser.data.username, jobId);
+  //     setHasApplied(false);
+  //   } catch (error) {
+  //     console.error("Error unapplying to job", error);
+  //   }
+  // }
+
+  return (
+    <div
+      className="
+        card
+      bg-gray-100
+        shadow-lg
+        h-52
+        w-[70vw]
+        md:w-[50vw]
+        lg:w-[40vw]
+        xl:w-[30vw]
+        py-4
+        mt-8
+        hover:scale-105
+        ease-in
+        duration-200
+      "
+    >
+      <div className="card-body">
+        <h3 className="card-title text-black">{job.title}</h3>
+        <h4 className="text-black">{job.companyName}</h4>
+        {job.salary && (
+          <small className="text-black">{`Salary: $${job.salary}`}</small>
+        )}
+        {job.equity && (
+          <small className="text-black">{`Equity: ${job.equity}`}</small>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/containers/Job/JobList/index.tsx
@@ -0,0 +1,49 @@
+/** JobList.
+ *
+ * @description
+ * Displays list of all jobs in database.
+ *
+ * State:
+ * - jobs: [ { id, title, salary, equity }, ... ]
+ * TODO: fix types for API.
+ */
+
+import { useState, useEffect } from "react";
+
+import JoblyApi from "../../../utils/api";
+import CardList from "../../../components/Job/CardList";
+import { JobI } from "../../../utils/definitions";
+
+export default function JobList() {
+  const [jobs, setJobs] = useState<Array<JobI> | null>(null);
+
+  useEffect(() => {
+    async function fetchJobs() {
+      const data = await JoblyApi.getJobs("");
+      setJobs(data);
+    }
+    fetchJobs();
+  }, []);
+
+  //TODO: create better loading screen.
+  if (!jobs) return <h1>Loading...</h1>;
+
+  return (
+    <div
+      className="
+        m-5
+        flex
+        flex-col
+        items-center
+        justify-center
+        md:grid
+        md:grid-cols-2
+        lg:grid-cols-3
+        bg-[#141550]
+      "
+    >
+      {jobs.length === 0 && <p>Sorry, no results were found!</p>}
+      <CardList jobs={jobs} />
+    </div>
+  );
+}
