6a9556efa6231d6f18589fd2aad6afb70354a493
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-07-11 18:19:59 -0700

    created button for liking/unliking jobs, but no functionality yet

--- src/containers/Job/JobDetail/index.tsx
+++ src/containers/Job/JobDetail/index.tsx
@@ -7,21 +7,25 @@
  * - job: { id, title, salary, equity, companyHandle, companyName }
  *
  * TODO:
- * - add feature to like/unlike jobs for applying.
+ * - add button to show interest in a job.
  */
 
-// import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { JobDetailsI } from "../../../utils/definitions";
 
 export default function JobDetails({ job }: JobDetailsI) {
-  // const [hasApplied, setHasApplied] = useState(false);
+  const [isInterested, setIsInterested] = useState(false);
 
   // useEffect(() => {
   //   if (hasAppliedToJob(job.id)) {
   //     setHasApplied(true);
   //   }
   // }, [job.id, hasAppliedToJob]);
+
+  const handleClick = () => {
+    setIsInterested(!isInterested);
+  };
 
   // /** handles onClick for liking jobs. */
   // async function handleApply(evt) {
@@ -68,6 +72,37 @@ export default function JobDetails({ job }: JobDetailsI) {
         {job.equity && (
           <small className="text-black">{`Equity: ${job.equity}`}</small>
         )}
+        <button
+          className={`
+            btn
+            btn-sm
+            btn-circle
+            ${isInterested ? "btn-success" : "btn-error"}
+            absolute
+            bottom-4
+            right-4
+          `}
+          onClick={handleClick}
+          value={job.id}
+          aria-label={`${
+            isInterested ? "press to unlike job" : "press to like job"
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
