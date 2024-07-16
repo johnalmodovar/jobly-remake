e74675b32edbe28476825c8c25c53e74b3974d87
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-07-16 12:50:09 -0700

    created SearchForm component for searching functionality

---
+++ src/containers/Form/Search/index.tsx
@@ -0,0 +1,38 @@
+/** SearchForm.
+ *
+ * @description
+ * Form that filters input from user to search for a company or job.
+ */
+
+import { useState } from "react";
+
+export default function SearchForm() {
+  const [formData, setFormData] = useState("");
+
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData(evt.target.value);
+  };
+
+  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+  };
+
+  return (
+    <form className="form-control" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search"
+        className="
+          bg-gray-100
+          input
+          input-bordered
+          w-24
+          md:w-auto
+          h-9
+        "
+        value={formData}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
