2be723611f8c65151f07caa2c8dfbcbe7da9027a
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-07-15 20:28:06 -0700

    edited placeholder for error page

--- src/Router/constants.tsx
+++ src/Router/constants.tsx
@@ -1,15 +1,10 @@
 import { RouterI } from "../types";
-import Homepage from "../containers/Homepage";
+
 import CompanyList from "../containers/Company/CompanyList";
 import CompanyPage from "../containers/Company/CompanyPage";
 import JobList from "../containers/Job/JobList";
 
 export const routerData: RouterI[] = [
-  {
-    path: "",
-    title: "home",
-    element: <Homepage />,
-  },
   {
     path: "/companies",
     title: "companies",
