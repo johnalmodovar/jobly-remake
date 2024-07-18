1d8556475636ad1ea0639165ed0b6d8c9522865a
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-07-17 19:39:00 -0700

    imported stores to be able to grab jobs that the user has liked

--- src/containers/UserProfile/JobLikes/index.tsx
+++ src/containers/UserProfile/JobLikes/index.tsx
@@ -4,6 +4,30 @@
  * Page that shows all the jobs the user has liked.
  */
 
+import { useState, useEffect } from "react";
+
+import { useJobStore, useUserStore } from "../../../states/stores";
+import { JobI } from "../../../types";
+import CardList from "../../../components/Job/CardList";
+
 export default function JobLikes() {
-  return;
+  const { user } = useUserStore();
+  const { jobs, fetchJobs } = useJobStore();
+
+  const [userJobs, setUserJobs] = useState<Array<JobI>>([]);
+
+  useEffect(() => {
+    fetchJobs();
+    const userJobs = jobs.filter((job) => user.likes.includes(job.id));
+    setUserJobs(userJobs);
+  }, [user.likes]);
+
+  return (
+    <div>
+      {userJobs.length === 0 && (
+        <p>You haven't liked any jobs yet. Start job hunting!</p>
+      )}
+      <CardList jobs={userJobs} />
+    </div>
+  );
 }
