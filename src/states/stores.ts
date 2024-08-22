0427c5c36e2f762d00fae33ef211e85bf13da214
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-08-22 16:04:17 -0700

    added deployed backend url

--- src/states/stores.ts
+++ src/states/stores.ts
@@ -14,7 +14,7 @@ import {
 } from "../types";
 
 //TODO: after deploying backend, put URL here.
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://jobly-server.vercel.app/" || "http://localhost:3001";
 
 export const useCompanyStore = create<CompanyStoreI>((set) => ({
   company: null,
